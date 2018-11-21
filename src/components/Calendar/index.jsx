b4e78bfff76b86405181d82d48c0f6f91b62167c
Hanif Carroll <hanifcarroll@gmail.com>
2018-11-21 10:08:16 -0500

    Center times and calendar

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactCalendar from "react-calendar";
+import styles from "./styles.module.scss";
 
 export const Calendar = ({ onChange }) => {
-  return <ReactCalendar onChange={onChange} />;
+  return (
+    <div className={styles.container}>
+      <ReactCalendar className={styles.calendar} onChange={onChange} />
+    </div>
+  );
 };
--- src/components/Times/index.jsx
+++ src/components/Times/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from "./styles.module.scss";
 
 export const Times = ({ times }) => {
   const { sunrise, noon, sunset } = times;
 
   return (
-    <div className="times">
+    <div className={styles.times}>
       <p>Sunrise - {sunrise}</p>
       <p>Solar Noon - {noon}</p>
       <p>Sunset - {sunset}</p>
