fc67ef5f74120f750ffd3ce28f9fd6dc68d9b6b1
Hanif Carroll <hanifcarroll@gmail.com>
2018-11-20 21:03:01 -0500

    Add support for choosing date.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Axios from "axios";
+import Calendar from "react-calendar";
+import { format } from "date-fns";
 
 class App extends Component {
+  state = {
+    lat: null,
+    lng: null,
+    date: null,
+  };
+
+  getData = async () => {
+    let url = `https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400`;
+
+    if (this.state.date) {
+      url += `&date=${this.state.date}`;
+    }
+
+    const {
+      data: { results },
+    } = await Axios.get(url).catch(err => console.log(err));
+
+    console.log(results);
+  };
+
+  onCalendarChange = date => {
+    date = format(date, "YYYY-MM-DD");
+    this.setState({ date });
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>App</h1>
+        <Calendar onChange={date => this.onCalendarChange(date)} />
+        <button onClick={this.getData}>Get Data</button>
       </div>
     );
   }
