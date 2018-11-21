9f80beb4845e5a1bbc7e5f21be233c43c71f0331
Hanif Carroll <hanifcarroll@gmail.com>
2018-11-21 09:28:21 -0500

    Add location name to DOM

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { Component } from "react";
 import Axios from "axios";
 import Calendar from "react-calendar";
 import { format } from "date-fns";
+import { APIKEY, RESULTSLIMIT } from "./config";
 
 class App extends Component {
   state = {
-    lat: null,
-    lng: null,
     location: "",
     geoInfo: {},
     date: null,
@@ -20,10 +19,8 @@ class App extends Component {
           coords: { latitude, longitude },
         } = position;
 
-        const apiKey = "9e59bc25-9417-438f-860e-926289f82523";
-        const resultsLimit = 1;
         const point = `${latitude},${longitude}`;
-        const url = `https://graphhopper.com/api/1/geocode?point=${point}&limit=${resultsLimit}&reverse=true&key=${apiKey}`;
+        const url = `https://graphhopper.com/api/1/geocode?point=${point}&limit=${RESULTSLIMIT}&reverse=true&key=${APIKEY}`;
 
         const {
           data: { hits: results },
@@ -37,15 +34,17 @@ class App extends Component {
   setLocation = () => {
     const { location } = this.state;
 
+    if (!location) {
+      return;
+    }
+
     const locationURI = encodeURIComponent(location);
 
     this.geocodeLocation(locationURI);
   };
 
   geocodeLocation = async location => {
-    const apiKey = "9e59bc25-9417-438f-860e-926289f82523";
-    const resultsLimit = 1;
-    const url = `https://graphhopper.com/api/1/geocode?q=${location}&limit=${resultsLimit}&key=${apiKey}`;
+    const url = `https://graphhopper.com/api/1/geocode?q=${location}&limit=${RESULTSLIMIT}&key=${APIKEY}`;
 
     const {
       data: { hits: results },
@@ -57,9 +56,9 @@ class App extends Component {
   extractGeoData = results => {
     if (results[0]) {
       console.log(results[0]);
-      const { country, state, name, point } = results[0];
+      const { country, state, city, name, point } = results[0];
 
-      this.setState({ geoInfo: { country, state, name, point } });
+      this.setState({ geoInfo: { country, state, city, name, point } });
     } else {
       console.log("Query not found.");
       this.setState({ geoInfo: {} });
@@ -77,7 +76,10 @@ class App extends Component {
   };
 
   makeSunUrl = () => {
-    const { lat, lng, date } = this.state;
+    const { geoInfo, date } = this.state;
+    const {
+      point: { lat, lng },
+    } = geoInfo;
 
     let url = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&formatted=0`;
 
@@ -89,6 +91,7 @@ class App extends Component {
   };
 
   extractTimes = results => {
+    console.log(results);
     const sunrise = new Date(results.sunrise).toLocaleTimeString();
     const sunset = new Date(results.sunset).toLocaleTimeString();
     const noon = new Date(results.solar_noon).toLocaleTimeString();
@@ -101,8 +104,25 @@ class App extends Component {
     this.setState({ date });
   };
 
+  renderLocation = () => {
+    const {
+      geoInfo: { country, state, city, name },
+    } = this.state;
+
+    let locationString = "";
+    name ? (locationString += `${name}`) : (locationString += "");
+    city && city !== name
+      ? (locationString += `, ${city}`)
+      : (locationString += "");
+    state ? (locationString += `, ${state}`) : (locationString += "");
+    country ? (locationString += `, ${country}`) : (locationString += "");
+
+    return <p>{locationString}</p>;
+  };
+
   render() {
-    const { sunrise, sunset, noon, location } = this.state.times;
+    const { location, times } = this.state;
+    const { sunrise, sunset, noon } = times;
 
     return (
       <div className="App">
@@ -115,10 +135,13 @@ class App extends Component {
           value={location}
           onChange={e => this.setState({ location: e.target.value })}
         />
-        <button onClick={this.setLocation}>Use Input Location</button>
+        <button onClick={this.setLocation} disabled={!location}>
+          Use Input Location
+        </button>
         {console.log(this.state)}
+        {this.renderLocation()}
         <p>Sunrise - {sunrise}</p>
-        <p>Noon - {noon}</p>
+        <p>Solar Noon - {noon}</p>
         <p>Sunset - {sunset}</p>
       </div>
     );
